39bd9c89604252e944212f586fd77b34b49872ca
Clint Olson <coandco@gmail.com>
2016-10-27 15:48:19 -0700

    Add definition for Always Human.

---
+++ seq/Always_Human/index.php
@@ -0,0 +1,12 @@
+<?php
+$comicname = "Always Human";
+$fileprefix = "ah";
+$filetype = "png";
+$perpage = "5";
+$padamount = "4";
+$elementpad = "3";
+$archivename = "";
+$backcolor = "white";
+
+?>
+<?php include "../include/pagedmultiple.php"; ?>
