2be009461a71f8ecc0c3ca2d036b527d539c14bf
Clint Olson <coandco@gmail.com>
2016-10-27 15:47:21 -0700

    Add definition for A Miracle of Science.

---
+++ seq/A_Miracle_of_Science/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "A Miracle of Science";
+$fileprefix = "mos";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "3";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
