96171747d511599d57fd7a34f42abdfbcdeaf9b0
Clint Olson <coandco@gmail.com>
2016-10-27 16:59:04 -0700

    Add definition for Dan and Mab's Furry Adventure.

---
+++ seq/Dan_and_Mab's_Furry_Adventure/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Dan and Mab's Furry Adventures";
+$fileprefix = "dmfa";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = 'Dan_and_Mab\'s_Furry_Adventure';
+?>
+<?php include "../include/paged.php"; ?>
