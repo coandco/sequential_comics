13202e8d7ac08404bec01d7e5188f2158e3c4b97
Clint Olson <coandco@gmail.com>
2016-10-27 15:53:12 -0700

    Add definition for Atland.

---
+++ seq/Atland/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Atland";
+$fileprefix = "atland";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "3";
+$archivename = "Atland";
+?>
+<?php include "../include/paged.php"; ?>
