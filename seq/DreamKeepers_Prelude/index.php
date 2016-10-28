fd4b5042f10ac8fe9fdd0dc84594b7731f1eb6cb
Clint Olson <coandco@gmail.com>
2016-10-27 17:23:36 -0700

    Add definition for DreamKeepers: Prelude.

---
+++ seq/DreamKeepers_Prelude/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "DreamKeepers Prelude";
+$fileprefix = "dkp";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
