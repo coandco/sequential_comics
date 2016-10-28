1baf00112825e0c7008ede9138a1623316bbb17d
Clint Olson <coandco@gmail.com>
2016-10-28 12:22:52 -0700

    Added definition for Inherit the Earth.

---
+++ seq/Inherit_the_Earth/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Inherit the Earth";
+$fileprefix = "inherit";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "Inherit_the_Earth";
+?>
+<?php include "../include/paged.php"; ?>
