71dff8b38ba5207ca9a008c61b816648e9d1dc72
Clint Olson <coandco@gmail.com>
2016-10-27 18:02:25 -0700

    Add Dream*Scar definition.

---
+++ seq/Dream_Scar/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Dream*Scar";
+$fileprefix = "dscar";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+?>
+<?php include "../include/pagedblack.php"; ?>
