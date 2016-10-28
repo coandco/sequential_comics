9b1b77f0e16fa254f1183f50112fa73e6700b2c2
Clint Olson <coandco@gmail.com>
2016-10-27 20:23:38 -0700

    Add definition for Furry Experience.

---
+++ seq/Furry_Experience/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Furry Experience";
+$fileprefix = "fexp";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
