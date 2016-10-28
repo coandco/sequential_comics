63bd14030456f35a34f30336dd95ec04ff6040a9
Clint Olson <coandco@gmail.com>
2016-10-27 21:21:46 -0700

    Added definition for Girls With Slingshots.

---
+++ seq/Girls_With_Slingshots/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Girls With Slingshots";
+$fileprefix = "gws";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
