f91bc53bf28dbcec09137a2e9c7ec19be4fa58a0
Clint Olson <coandco@gmail.com>
2016-10-27 19:52:10 -0700

    Added definitions for Friendship Is Betrayal.

---
+++ seq/Friendship_Is_Betrayal/index.php
@@ -0,0 +1,10 @@
+<?php
+$comicname = "My Little Pony: Friendship Is Betrayal";
+$fileprefix = "fib";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+
+?>
+<?php include "../include/paged.php"; ?>
