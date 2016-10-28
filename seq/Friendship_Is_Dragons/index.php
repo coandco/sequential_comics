c30909cb4f01a1a4e90c8ffd12bf85921e7a3809
Clint Olson <coandco@gmail.com>
2016-10-27 20:01:51 -0700

    Added definition for Friendship is Dragons.

---
+++ seq/Friendship_Is_Dragons/index.php
@@ -0,0 +1,10 @@
+<?php
+$comicname = "Friendship Is Dragons";
+$fileprefix = "fid";
+$filetype = "png";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+
+?>
+<?php include "../include/paged.php"; ?>
