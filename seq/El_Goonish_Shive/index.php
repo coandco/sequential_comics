94df5eab783e70f9e28a55dbda6639109c3478bf
Clint Olson <coandco@gmail.com>
2016-10-27 18:03:46 -0700

    Add definition for El Goonish Shive.

---
+++ seq/El_Goonish_Shive/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "El Goonish Shive";
+$fileprefix = "egs";
+$filetype = "gif";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
