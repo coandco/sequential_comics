70888304feb7325d98ced2be6e3e908409f4a640
Clint Olson <coandco@gmail.com>
2016-10-28 12:28:02 -0700

    Add definition for Inverloch.

---
+++ seq/Inverloch/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Inverloch";
+$fileprefix = "inverloch";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "3";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
