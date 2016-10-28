a9bbaa511d5c7d9543c2cb8b84b2be68459da444
Clint Olson <coandco@gmail.com>
2016-10-27 22:16:24 -0700

    Added Gunnerkrigg Court definition.

---
+++ seq/Gunnerkrigg_Court/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Gunnerkrigg Court";
+$fileprefix = "gunner";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "8";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
