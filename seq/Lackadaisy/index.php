adf16caed6330142cd9ad636feaf0cc1cc0d43ed
Clint Olson <coandco@gmail.com>
2016-10-28 15:44:04 -0700

    Added definition for Lackadaisy Cats.

---
+++ seq/Lackadaisy/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Lackadaisy";
+$fileprefix = "lackadaisy";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "3";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
