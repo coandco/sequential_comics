c302d9d394d505c637b5452f02d4bdf3277af9cf
Clint Olson <coandco@gmail.com>
2016-10-28 18:52:10 -0700

    Add definition for Order of the Stick.

---
+++ seq/Order_of_the_Stick/index.php
@@ -0,0 +1,10 @@
+<?php
+$comicname = "Order of the Stick";
+$fileprefix = "oots";
+$filetype = "gif";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+
+?>
+<?php include "../include/paged.php"; ?>
