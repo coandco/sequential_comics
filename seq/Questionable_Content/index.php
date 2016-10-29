8685aa1aa4adbc8b169df9ad12f8e2a073e0af6b
Clint Olson <coandco@gmail.com>
2016-10-28 22:15:31 -0700

    Added definition for Questionable Content.

---
+++ seq/Questionable_Content/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Questionable Content";
+$fileprefix = "qc";
+$filetype = "png";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
