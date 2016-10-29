f2d87c26b20ad8101640de5134eb695ca9530b65
Clint Olson <coandco@gmail.com>
2016-10-29 00:35:50 -0700

    Add definition for Trying Human.

---
+++ seq/Trying_Human/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Trying Human";
+$fileprefix = "thuman";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
