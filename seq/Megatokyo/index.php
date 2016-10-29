1bb7095d056d4e8a57f2dd841923f1dbac904469
Clint Olson <coandco@gmail.com>
2016-10-28 17:07:02 -0700

    Add definition for Megatokyo.

---
+++ seq/Megatokyo/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Megatokyo";
+$fileprefix = "megatokyo";
+$filetype = "gif";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
