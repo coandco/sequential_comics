b976ed7b98b9a7e78432d0ef25f1b55352e33bcf
Clint Olson <coandco@gmail.com>
2016-10-29 10:37:26 -0700

    Add definition for WTF Comics: Smoke's Tale.

---
+++ seq/WTFSmoke/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "WTF Comics: Smoke's Tale";
+$fileprefix = "WTFSmoke";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
