2e6eefdd9dda0d289fa62db02365d3cc57aa6827
Clint Olson <coandco@gmail.com>
2016-10-29 11:37:05 -0700

    Add definition for Winters in Lavelle.

---
+++ seq/Winters_in_Lavelle/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Winters in Lavelle";
+$fileprefix = "lavelle";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
