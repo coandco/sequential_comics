5f356b7247912e5145e016afc8231831522fbef1
Clint Olson <coandco@gmail.com>
2016-10-29 12:37:43 -0700

    Add definition for Freefall.

---
+++ seq/Freefall/index.php
@@ -0,0 +1,11 @@
+<?php
+$comicname = "Freefall Color";
+$fileprefix = "fc";
+$filetype = "png";
+$perpage = "50";
+$padamount = "5";
+$archivename = "Freefall";
+$archivestart = "1252";
+?>
+<?php include "../include/paged.php"; ?>
+
