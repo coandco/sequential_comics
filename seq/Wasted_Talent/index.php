9741b42061f2c99e33b433d026b30e1c141824c8
Clint Olson <coandco@gmail.com>
2016-10-29 11:36:09 -0700

    Add definition for Wasted Talent.

---
+++ seq/Wasted_Talent/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "Wasted Talent";
+$fileprefix = "wasted";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
