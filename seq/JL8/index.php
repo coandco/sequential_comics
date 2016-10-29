80a1ae4ada2ab789d5fcaf0d28621d1d403eea8c
Clint Olson <coandco@gmail.com>
2016-10-29 12:55:32 -0700

    Add definition for JL8.

---
+++ seq/JL8/index.php
@@ -0,0 +1,12 @@
+<?php
+$comicname = "JL8";
+$fileprefix = "jleight";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$elementpad = "3";
+$archivename = "";
+$backcolor = "white";
+
+?>
+<?php include "../include/pagedmultiple.php"; ?>
