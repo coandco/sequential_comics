f796e570cbaf9b9d1bedcd77fe235dc4f461bc81
Clint Olson <coandco@gmail.com>
2016-10-29 00:09:35 -0700

    Add definition for The Probability Broach.

---
+++ seq/The_Probability_Broach/index.php
@@ -0,0 +1,9 @@
+<?php
+$comicname = "The Probability Broach";
+$fileprefix = "TPB";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "3";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
