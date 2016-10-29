72605c41fd4749309a449f76e80849cf31e35eaf
Clint Olson <coandco@gmail.com>
2016-10-28 18:18:34 -0700

    Add definition for Murphy's Law.

---
+++ seq/Murphys_Law/index.php
@@ -0,0 +1,10 @@
+<?php
+$comicname = "Murphy's Law";
+$fileprefix = "murphy";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+
+?>
+<?php include "../include/paged.php"; ?>
