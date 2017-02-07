fcdaf2868bdafeda733be957192b0e9aa448296d
Clint Olson <coandco@gmail.com>
2017-02-06 19:17:46 -0800

    Add definition for A Tale of Two Rulers.

---
+++ seq/A_Tale_of_Two_Rulers/index.php
@@ -0,0 +1,14 @@
+<?php
+$comicname = "A Tale of Two Rulers";
+$comiclink = 'http://figmentforms.tumblr.com/post/139806271367/a-tale-of-two-rulers-archive-post';
+$maxwidth = "900";
+$fileprefix = "tworulers";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$elementpad = "3";
+$archivename = "";
+$backcolor = "white";
+
+?>
+<?php include "../include/pagedmultiple.php"; ?>
