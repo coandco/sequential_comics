221ccb09507881a62eba3bec62d808e738379b87
Clint Olson <coandco@gmail.com>
2017-04-26 19:59:19 -0700

    Add definition for The Draconia Chronicles.

---
+++ seq/The_Draconia_Chronicles/index.php
@@ -0,0 +1,11 @@
+<?php
+$comicname = "Draconia";
+$comiclink = "http://draconia.katbox.net";
+$fileprefix = "draconia";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "3";
+$archivename = "";
+
+?>
+<?php include "../include/paged.php"; ?>
