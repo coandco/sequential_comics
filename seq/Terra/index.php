4a0a30a9115053e6cfeab61df34d09b5675c65d9
Clint Olson <coandco@gmail.com>
2017-04-26 20:28:15 -0700

    Add Terra definition.

---
+++ seq/Terra/index.php
@@ -0,0 +1,11 @@
+<?php
+$comicname = "Terra";
+$comiclink = "http://terra-comic.com";
+$fileprefix = "terra";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+
+?>
+<?php include "../include/paged.php"; ?>
