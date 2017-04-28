2cffeda92170c7ac1228d4ea708158c5e100ebae
Clint Olson <coandco@gmail.com>
2017-04-28 09:59:55 -0700

    Add Runewriters definition.

---
+++ seq/Runewriters/index.php
@@ -0,0 +1,11 @@
+<?php
+$comicname = "Runewriters";
+$comiclink = "http://runewriters.com/";
+$fileprefix = "runew";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+
+?>
+<?php include "../include/paged.php"; ?>
