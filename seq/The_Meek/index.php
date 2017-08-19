8529151782f0a5363c739ebcea6a65f4279c9a6c
Clint Olson <coandco@gmail.com>
2017-08-19 08:26:31 -0700

    Add definition for The Meek.

---
+++ seq/The_Meek/index.php
@@ -0,0 +1,12 @@
+<?php
+$comicname = "The Meek";
+$comiclink = "http://www.meekcomic.com/";
+$fileprefix = "meek";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+
+?>
+<?php include "../include/paged.php"; ?>
+
