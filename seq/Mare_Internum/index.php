94c92776ba7adc842a18d76a75bbe3ec2440366d
Clint Olson <coandco@gmail.com>
2017-08-18 20:59:29 -0700

    Add definition for Mare Internum.

---
+++ seq/Mare_Internum/index.php
@@ -0,0 +1,11 @@
+<?php
+$comicname = "Mare Internum";
+$comiclink = "http://www.marecomic.com/";
+$fileprefix = "mare";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+
+?>
+<?php include "../include/paged.php"; ?>
