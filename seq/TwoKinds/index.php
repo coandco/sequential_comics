d42dcc15fb4f30bc5b2309a7779c43be450a49eb
Clint Olson <coandco@gmail.com>
2017-11-03 14:46:29 -0700

    Added definition for TwoKinds (http://twokinds.keenspot.com)

---
+++ seq/TwoKinds/index.php
@@ -0,0 +1,11 @@
+<?php
+$comicname = "TwoKinds";
+$comiclink = "http://twokinds.keenspot.com/";
+$fileprefix = "twokinds";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+?>
+<?php include "../include/paged.php"; ?>
+
