48649c618e08f9a3a6cdc64a12151c89b11fcac9
Clint Olson <coandco@gmail.com>
2018-02-08 23:56:38 -0800

    Add definition for Slightly Damned.

---
+++ seq/Slightly_Damned/index.php
@@ -0,0 +1,12 @@
+<?php
+$comicname = "Slightly Damned";
+$comiclink = "http://www.sdamned.com/";
+$fileprefix = "sdamned";
+$filetype = "jpg";
+$perpage = "50";
+$padamount = "4";
+$archivename = "";
+
+?>
+<?php include "../include/paged.php"; ?>
+
